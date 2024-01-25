c21b17bfa7de34a52b065ec59f979a44f4361efc
Sushant <sushantp053@gmail.com>
2024-01-25 11:56:45 +0530

    models changes

--- home/models.py
+++ home/models.py
@@ -2,12 +2,15 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-    post_id = models.IntegerField(primary_key = True)
+    post_id = models.BigAutoField(primary_key = True)
     image = models.ImageField()
     text = models.TextField()
     user_id = models.ForeignKey(User, on_delete = models.CASCADE)
     posted_at = models.DateTimeField()
     location = models.TextField()
+
+    def __str__(self) -> str:
+        return self.text + self.location
 
 class Comments(models.Model):
     comment_id = models.IntegerField(primary_key = True)
@@ -21,3 +24,9 @@ class Like(models.Model):
     post_id = models.ForeignKey(Post, on_delete = models.CASCADE)
     user_id = models.ForeignKey(User, on_delete= models.DO_NOTHING)
     liked_at= models.DateTimeField()
+
+class ComComment(models.Model):
+    comId = models.BigAutoField(primary_key = True)
+    comment_id = models.ForeignKey(Comments, on_delete = models.CASCADE )
+    comment = models.TextField()
+    user_id = models.ForeignKey(User, on_delete = models.DO_NOTHING)
